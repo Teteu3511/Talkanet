4b50d5579139754c71754a702d93f30fc178e0b0
Teteu3511 <leaesmatheus10@gmail.com>
2024-11-28 11:45:17 -0300

    Changed changes

---
+++ post.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28/11/2024 às 13:41
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc-talkanet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `content` varchar(400) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `post`
+--
+
+INSERT INTO `post` (`id`, `username`, `content`) VALUES
+(2, 'KarekaLinds', 'Procuro hambúrgueres de siri.'),
+(3, 'Kleiton', 'Algúem pra me dar o CPT RONC na promo?');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- templates/header.php
+++ templates/header.php
@@ -30,24 +30,26 @@
 
 	<body>
 
-		<section class="header-wrapper">
-			<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" id="header">
-      			<a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        			<span class="fs-4" id="header-title">Talkanet</span>
-      			</a>
-				
-				<div class="search-container">
-					<form class="w-100 me-5" role="search">
-          				<input type="search" class="form-control"  id="search-input" name="search-input" placeholder="Pesquisar..." aria-label="Search">
-        			</form>
-				</div>
+		<section class="page-control">
 
-      			<ul class="nav nav-pills" id="navbar">
-        			<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-        			<li class="nav-item"><a href="add_post.php" class="nav-link">Postar</a></li>
-        			<li class="nav-item"><a href="help.php" class="nav-link">Ajuda</a></li>
-        			<li class="nav-item"><a href="aboutus.php" class="nav-link">Sobre nós</a></li>
-      			</ul>
-			
-    		</header>
-		</section>
+			<section class="header-wrapper">
+				<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" id="header">
+					<a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+						<span class="fs-4" id="header-title">Talkanet</span>
+					</a>
+					
+					<div class="search-container">
+						<form class="w-100 me-5" role="search">
+							<input type="search" class="form-control"  id="search-input" name="search-input" placeholder="Pesquisar..." aria-label="Search">
+						</form>
+					</div>
+
+					<ul class="nav nav-pills" id="navbar">
+						<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+						<li class="nav-item"><a href="add_post.php" class="nav-link">Postar</a></li>
+						<li class="nav-item"><a href="help.php" class="nav-link">Ajuda</a></li>
+						<li class="nav-item"><a href="aboutus.php" class="nav-link">Sobre nós</a></li>
+					</ul>
+				
+				</header>
+			</section>
