0299d650980051a026788a850a2e90a072fa1054
kaike-codes <kaike.andreonedarosa@gmail.com>
2024-11-28 11:00:49 -0300

    hi

--- templates/header.php
+++ templates/header.php
@@ -14,7 +14,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR" data-bs-theme="dark">
+<html lang="pt-BR">
 
 	<head>
 
@@ -33,20 +33,21 @@
 		<section class="header-wrapper">
 			<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" id="header">
       			<a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        			<span class="fs-4">Talkanet</span>
+        			<span class="fs-4" id="header-title">Talkanet</span>
       			</a>
-
+				
 				<div class="search-container">
 					<form class="w-100 me-5" role="search">
           				<input type="search" class="form-control"  id="search-input" name="search-input" placeholder="Pesquisar..." aria-label="Search">
         			</form>
 				</div>
 
-      			<ul class="nav nav-pills">
+      			<ul class="nav nav-pills" id="navbar">
         			<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
         			<li class="nav-item"><a href="add_post.php" class="nav-link">Postar</a></li>
         			<li class="nav-item"><a href="help.php" class="nav-link">Ajuda</a></li>
         			<li class="nav-item"><a href="aboutus.php" class="nav-link">Sobre nós</a></li>
       			</ul>
+			
     		</header>
 		</section>
