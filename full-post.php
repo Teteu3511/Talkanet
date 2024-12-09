401f81bad7408502e06838600056ad76aadc164d
Teteu3511 <leaesmatheus10@gmail.com>
2024-12-09 11:44:33 -0300

    ajhsjhasjh

--- full-post.php
+++ full-post.php
@@ -20,7 +20,7 @@
 
             <div class="list-group">
 
-                <div id="posts-container">
+                <div id="single-post-container" class="list-group-item d-flex flex-md-column gap-3 py-3">
                         
                     <img src="<?= $BASE_URL ?>img/profile.png" alt="Perfil" class="rounded-circle flex-shrink-0" id="profile-icon">
 
@@ -28,37 +28,36 @@
 
                     <p><?= $post["content"] ?></p>
 
+
                 </div>
+
+                <form id="send-comment" action="<?php $BASE_URL ?>config/process.php" method="POST">
+
+                    <input type="hidden" name="type" value="comment">
+                    <input type="hidden" name="post_id" value="<?= $id ?>">
+
+                    <div class="send-section">
+
+                        <label for="uname" class="send-label" id="user-label">Nome de usuário:</label>
+                        <input class="send-box" id="uname" name="username" placeholder="Insira seu nome de usuário" required autocomplete="username">
+
+                    </div>
+
+                    <div class="send-section">
+
+                        <label for="ctnt" class="send-label">Escreva seu comentário:</label>
+                        <input class="send-box" id="cmmt" name="comment" placeholder="Escreva seu comentário" required autocomplete="off">
+                        <button type="submit" id="send-submit"><i class="fa-solid fa-paper-plane"></i></button>
+
+                    </div>
+
+                </form>
 
             </div>
 
         </div>
 
-        <div class="send-post-container">
 
-            <form id="send-comment" action="<?php $BASE_URL ?>config/process.php" method="POST">
-
-                <input type="hidden" name="type" value="comment">
-                <input type="hidden" name="post_id" value="<?= $id ?>">
-
-                <div class="send-section">
-
-                    <label for="uname" class="send-label" id="user-label">Nome de usuário:</label>
-                    <input class="send-box" id="uname" name="username" placeholder="Insira seu nome de usuário" required autocomplete="username">
-
-                </div>
-
-                <div class="send-section">
-
-                    <label for="ctnt" class="send-label">Escreva seu comentário:</label>
-                    <input class="send-box" id="cmmt" name="comment" placeholder="Escreva seu comentário" required autocomplete="off">
-                    <button type="submit" id="send-submit"><i class="fa-solid fa-paper-plane"></i></button>
-
-                </div>
-
-            </form>
-
-        </div>
 
         <?php if(count($comments) > 0): ?>
 
@@ -66,7 +65,7 @@
 
                 <div class="list-group">
 
-                    <div id="posts-container">
+                    <div id="single-post-container" class="list-group-item d-flex flex-md-column gap-3 py-3">
 
                         <?php foreach ($comments as $comment): ?>
             
@@ -75,6 +74,8 @@
                             <h1 id="main-title"><?= $comment["username"] ?></h1>
 
                             <p><?= $comment["comment"] ?></p>
+
+                            <hr>
 
                         <?php endforeach; ?>
 
